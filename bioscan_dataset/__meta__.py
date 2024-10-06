0c5442b8c0d62aa996b753a89dab000bfaf6e9a3
Scott Lowe <scott.code.lowe@gmail.com>
2024-10-05 23:03:02 -0400

    Initialise project from template repository

---
+++ bioscan_dataset/__meta__.py
@@ -0,0 +1,8 @@
+name = "bioscan-dataset"
+path = name.lower().replace("-", "_").replace(" ", "_")
+version = "0.1.dev0"
+author = "Scott C. Lowe"
+author_email = "scott.code.lowe@gmail.com"
+description = "PyTorch torchvision-style datasets for BIOSCAN-1M and BIOSCAN-5M."
+url = ""
+license = "MIT"
