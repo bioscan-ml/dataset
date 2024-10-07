d878315f5d36f2d9b60272d22df2b541ef1fd43a
Scott Lowe <scott.code.lowe@gmail.com>
2024-10-07 00:48:27 -0400

    ENH: Add support for downloading cropped_256 images

--- bioscan_dataset/bioscan5m.py
+++ bioscan_dataset/bioscan5m.py
@@ -138,8 +138,10 @@ class BIOSCAN5M(VisionDataset):
         Label transformation pipeline.
 
     download : bool, default=False
-        If true, downloads the metadata from the internet and puts it in root directory.
-        If metadata is already downloaded, it is not downloaded again.
+        If true, downloads the dataset from the internet and puts it in root directory.
+        If dataset is already downloaded, it is not downloaded again.
+        Images are only downloaded if the ``"image"`` modality is requested.
+        Note that only ``image_package=cropped_256`` is supported for automatic image download.
     """
 
     base_folder = "bioscan5m"
@@ -151,6 +153,67 @@ class BIOSCAN5M(VisionDataset):
         "filename": os.path.join("metadata", "csv", "BIOSCAN_5M_Insect_Dataset_metadata.csv"),
         "archive_md5": "ac381b69fafdbaedc2f9cfb89e3571f7",
         "csv_md5": "603020b433ef566946efba7a08dbb23d",
+    }
+    zip_files = {
+        "eval": {
+            "cropped_256": {
+                "url": "https://zenodo.org/records/11973457/files/BIOSCAN_5M_cropped_256_eval.zip",
+                "md5": "16a997f72a8cd08cbcf7becafe2dda50",
+            },
+        },
+        "pretrain01": {
+            "cropped_256": {
+                "url": "https://zenodo.org/records/11973457/files/BIOSCAN_5M_cropped_256_pretrain.01.zip",
+                "md5": "0e1cfa86dc7fa4d9c10036990992a2dd",
+            },
+        },
+        "pretrain02": {
+            "cropped_256": {
+                "url": "https://zenodo.org/records/11973457/files/BIOSCAN_5M_cropped_256_pretrain.02.zip",
+                "md5": "ca3191d307957732e8108121b20a2059",
+            },
+        },
+        "train": {
+            "cropped_256": {
+                "url": "https://zenodo.org/records/11973457/files/BIOSCAN_5M_cropped_256_train.zip",
+                "md5": "3f170db5d95610644883dafc73389049",
+            },
+        },
+    }
+    image_files = {
+        "pretrain": [
+            ("pretrain/00/AACTA1040-20.jpg", {"cropped_256": "053311d26acb22a4726b867e558147ce"}),
+            ("pretrain/ff/YGEN686-22.jpg", {"cropped_256": "053df72d6162d7d7512bc6d5571b52bd"}),
+            ("pretrain/fe/YGEN2059-22.jpg", {"cropped_256": "5e0798d46efb11f6450150d07d185e7d"}),
+        ],
+        "train": [
+            ("train/0/AACTA1026-20.jpg", {"cropped_256": "a0746c803d07cbc7a87d808db763b8dc"}),
+            ("train/f/YGEN986-22.jpg", {"cropped_256": "42f3e0873cd924a0084adb31eb263fd2"}),
+        ],
+        "val": [
+            ("val/AACTA161-20.jpg", {"cropped_256": "43834d843e992b546abbcb0c6d6f0d43"}),
+            ("val/YGEN925-22.jpg", {"cropped_256": "c523319d0201306bd6fe530c362efeef"}),
+        ],
+        "test": [
+            ("test/AACTA1436-20.jpg", {"cropped_256": "dbe6de711585d84d5dd526dd42ce735d"}),
+            ("test/YGEN995-22.jpg", {"cropped_256": "e369efbe1ef0a4259fc8ba42d8cb4b4c"}),
+        ],
+        "key_unseen": [
+            ("key_unseen/AACTA5263-20.jpg", {"cropped_256": "856411c5ecfa238ce2dac607a1207784"}),
+            ("key_unseen/YGEN2149-22.jpg", {"cropped_256": "6d804a23ae6582e7d0ce9f296ad4390e"}),
+        ],
+        "val_unseen": [
+            ("val_unseen/ABOTH8801-22.jpg", {"cropped_256": "31e04be94df9f470ea79ff1266e36888"}),
+            ("val_unseen/YGEN2173-22.jpg", {"cropped_256": "8f63d89962ed4092bd205274bc24036f"}),
+        ],
+        "test_unseen": [
+            ("test_unseen/BCMIN4148-23.jpg", {"cropped_256": "d3542d6cc943256723a1a39a18f845e5"}),
+            ("test_unseen/YDBB974-21.jpg", {"cropped_256": "9cff547b03468b1552fb1647001d202f"}),
+        ],
+        "other_heldout": [
+            ("other_heldout/0/ABSSI1639-22.jpg", {"cropped_256": "82506b391cc33aa08b86f4c930c401a1"}),
+            ("other_heldout/f/YDBB6502-21.jpg", {"cropped_256": "0ea33145fcb57997605ea450a979e5d2"}),
+        ],
     }
 
     def __init__(
@@ -197,7 +260,7 @@ class BIOSCAN5M(VisionDataset):
         if download:
             self.download()
 
-        if not self._check_exists():
+        if not self._check_integrity():
             raise EnvironmentError(f"{type(self).__name__} dataset not found in {self.root}.")
 
         self.metadata = self._load_metadata()
@@ -240,22 +303,39 @@ class BIOSCAN5M(VisionDataset):
         p = self.metadata_path
         check = check_integrity(p, self.meta["csv_md5"])
         if verbose >= 1 and not check:
-            print(f"File missing: {p}")
+            if not os.path.exists(p):
+                print(f"File missing: {p}")
+            else:
+                print(f"File invalid: {p}")
         if verbose >= 2 and check:
             print(f"File present: {p}")
         return check
 
-    def _check_integrity_images(self, verbose=1) -> bool:
-        p = self.image_dir
-        check = os.path.isdir(p)
-        if verbose >= 1 and not check:
-            print(f"Directory missing: {p}")
-        if verbose >= 2 and check:
-            print(f"Directory present: {p}")
-        return check
+    def _check_integrity_images(self, split=None, verbose=1) -> bool:
+        if split is None:
+            split = self.split
+        if split == "all":
+            return all(self._check_integrity_images(split=s, verbose=verbose) for s in self.image_files)
+        check_all = True
+        for file, data in self.image_files[split]:
+            file = os.path.join(self.image_dir, file)
+            if self.image_package in data:
+                check = check_integrity(file, data[self.image_package])
+            else:
+                check = os.path.exists(file)
+            if verbose >= 1 and not check:
+                if not os.path.exists(file):
+                    print(f"File missing: {file}")
+                else:
+                    print(f"File invalid: {file}")
+            if verbose >= 2 and check:
+                print(f"File present: {file}")
+            check_all &= check
+        return check_all
 
     def _check_integrity(self, verbose=1) -> bool:
-        """Check if the dataset is already downloaded and extracted.
+        """
+        Check if the dataset is already downloaded and extracted.
 
         Parameters
         ----------
@@ -269,20 +349,57 @@ class BIOSCAN5M(VisionDataset):
         """
         check = True
         check &= self._check_integrity_metadata()
-        if "images" in self.modality:
+        if "image" in self.modality:
             check &= self._check_integrity_images()
         return check
 
-    def _download_metadata(self) -> None:
+    def _download_metadata(self, verbose=1) -> None:
         if self._check_integrity_metadata():
-            print("Metadata CSV file already downloaded and verified")
+            if verbose >= 1:
+                print("Metadata CSV file already downloaded and verified")
             return
         download_and_extract_archive(self.meta["urls"][0], self.root, md5=self.meta["archive_md5"])
 
+    def _download_image_zip(self, partition_set, image_package=None) -> None:
+        if image_package is None:
+            image_package = self.image_package
+        if image_package not in self.zip_files[partition_set]:
+            raise NotImplementedError(
+                f"Automatic download of image_package={image_package} is not yet implemented."
+                " Please manually download and extract the zip files."
+            )
+        data = self.zip_files[partition_set][image_package]
+        download_and_extract_archive(data["url"], self.root, md5=data.get("md5"))
+
+    def _download_images(self, verbose=1) -> None:
+        if self._check_integrity_images():
+            if verbose >= 1:
+                print("Images already downloaded and verified")
+            return
+        if self.split in ("all", "pretrain"):
+            self._download_image_zip("pretrain01")
+            self._download_image_zip("pretrain02")
+        if self.split in ("all", "train"):
+            self._download_image_zip("train")
+        if self.split in (
+            "all",
+            "eval",
+            "val",
+            "test",
+            "key_unseen",
+            "val_unseen",
+            "test_unseen",
+            "other_heldout",
+        ):
+            self._download_image_zip("eval")
+
     def download(self) -> None:
+        """
+        Download and extract the data.
+        """
         self._download_metadata()
-        if "images" in self.modality:
-            raise NotImplementedError("Automatically downloading image packages is not yet implemented.")
+        if "image" in self.modality:
+            self._download_images()
 
     def _load_metadata(self) -> pd.DataFrame:
         """
