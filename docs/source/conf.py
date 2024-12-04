97053f530df7c35275e29718475ef76729ced3c4
Scott Lowe <scott.code.lowe@gmail.com>
2024-12-03 19:12:49 -0500

    DOC: Change theme from book to rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -129,6 +129,7 @@ extensions = [
     "sphinx.ext.ifconfig",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
 ]
 
 # Some extension features only available on later Python versions
@@ -186,7 +187,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_book_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
