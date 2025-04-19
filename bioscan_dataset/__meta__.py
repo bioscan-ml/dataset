e50c00a8069e7f491659ddc0473f1e39282d12b5
Scott Lowe <scott.code.lowe@gmail.com>
2025-04-19 17:31:23 +0100

    REL: Set github installed version to 1.3.dev0

--- bioscan_dataset/__meta__.py
+++ bioscan_dataset/__meta__.py
@@ -1,6 +1,6 @@
 name = "bioscan-dataset"
 path = name.lower().replace("-", "_").replace(" ", "_")
-version = "1.3.0"
+version = "1.3.dev0"
 author = "Scott C. Lowe"
 author_email = "scott.code.lowe@gmail.com"
 description = "PyTorch torchvision-style datasets for BIOSCAN-1M and BIOSCAN-5M."
